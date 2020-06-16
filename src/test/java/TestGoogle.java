547fc6f31dc8d62d27acf71f53bbbf3c3e23d998
Optic969 <optic969@tut.by>
2020-06-16 19:26:52 +0300

    Initiation commit

---
+++ src/test/java/TestGoogle.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import org.junit.jupiter.api.Tag;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TestGoogle {
+    @Tag("slow")
+    @Test
+    public void testAddMaxInteger() {
+        assertEquals(2147483646, Integer.sum(2147183646, 300000));
+    }
+
+    @Tag("fast")
+    @Test
+    public void testDivide() {
+        assertThrows(ArithmeticException.class, () -> {
+            Integer.divideUnsigned(42, 0);
+        });
+    }
+}
