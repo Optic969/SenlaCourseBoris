5e9f2e5e2ae2a8edba098a1f2a1f5591a96ec04a
Optic969 <optic969@tut.by>
2020-06-26 16:12:42 +0300

    Added check exception try, catch, finally,for spam directory

--- src/test/java/TestGoogle.java
+++ src/test/java/TestGoogle.java
@@ -44,20 +44,50 @@ public class TestGoogle {
         options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
         WebDriver driver =  new ChromeDriver(options);
         WebDriverWait driverWait = new WebDriverWait(driver, 10);
+        WebDriverWait wait2 = new WebDriverWait(driver, 5);
         driver.get("https://stackoverflow.com/users/signup?ssrc=head&returnurl=%2fusers%2fstory%2fcurrent%27");
         driver.findElement(By.xpath("//div[@id='openid-buttons']//button[1]")).click();
         driverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='identifierId']"))).sendKeys("optic969@gmail.com");
         driver.findElement(By.id("identifierNext")).click();
         driverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name='password']"))).sendKeys("totenkopf");
         driver.findElement(By.id("passwordNext")).click();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         ((JavascriptExecutor)driver).executeScript("window.open()");
         ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
         driver.switchTo().window(tabs.get(1));
         driver.get("https://mail.google.com/");
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.findElements(By.xpath("//*[@role='checkbox']")).get(1).click();
         driver.findElements(By.xpath(" //div[@class='T-I J-J5-Ji nX T-I-ax7 T-I-Js-Gs mA']//div[@class='asa']")).get(0).click();
+        driver.findElements(By.xpath("//*[@id=\":k6\"]")).get(0).click();
+        driver.findElements(By.xpath("//div[@class='TN bzz aHS-bnv']")).get(0).click();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        try {
+            wait2.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//*[@id=\":ln\"]/div[1]/span")));
+            driver.findElement(By.xpath("//*[@id=\":ln\"]/div[1]/span")).click();
+            driver.findElement(By.xpath("//*[@id=\":4\"]/div[2]/div[1]/div[1]/div/div/div[2]/div/div")).click();
+        }catch (Exception exception){
+            System.out.println("The spam directory is empty");
+        }finally {
+            driver.findElement(By.xpath("//a[@class='gb_D gb_Ra gb_i']")).click();
+            driver.findElement(By.xpath("//a[@id='gb_71']")).click();
+        }
+
+
+
+
+
+
+
+        //driver.findElement(By.xpath("//td[@class='TC']")).getText().equalsIgnoreCase("Ура, спама нет!");
+        
+        /*if(driver.findElement(By.xpath("//td[@class='TC']")).getText().equalsIgnoreCase("Ура, спама нет!")){
+            System.out.println("The spam directory is empty");
+        }else{
+            driver.findElement(By.xpath("//span[@class='T-Jo J-J5-Ji T-Jo-auq T-Jo-Jp']")).click();
+            driver.findElement(By.xpath("//div[@class='T-I J-J5-Ji aFi T-I-ax7 mA']//div[@class='Bn']")).click();
+        }
+
 
         // String selectLinkOpeninNewTab = Keys.chord(Keys.CONTROL,"t");
         //driver.findElement(By.linkText("https://mail.google.com/")).sendKeys(selectLinkOpeninNewTab);
